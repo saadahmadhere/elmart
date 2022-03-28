42acaa53bf3c97b570b63142c344a9c088495aa6
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-03-28 15:32:31 +0530

    add data-context.js

---
+++ src/Helper/Contexts/data-context.js
@@ -0,0 +1,27 @@
+import { useContext, createContext, useReducer } from "react";
+
+const ProductDataContext = createContext();
+const useData = () => useContext(ProductDataContext);
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "PRODUCTS":
+      return { ...state, products: action.payload };
+    default:
+      return state;
+  }
+};
+
+const initialState = { products: [], categories: [] };
+
+const ProductDataProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <ProductDataContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ProductDataContext.Provider>
+  );
+};
+
+export { ProductDataProvider, useData };
