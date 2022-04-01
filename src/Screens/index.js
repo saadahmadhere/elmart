8942eaa1c131274ac686d160f6768f3c554a8fb3
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-01 14:42:23 +0530

    update barrel imports

--- src/Screens/index.js
+++ src/Screens/index.js
@@ -1,2 +1,4 @@
 export { ProductScreen } from "./ProductsScreen/ProductsScreen";
 export { HomeScreen } from "./HomeScreen/HomeScreen";
+export { CartScreen } from "./CartScreen/CartScreen";
+export { EmptyCart } from "./Empty Cart/EmptyCart";
