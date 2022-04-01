b0e2559ffbbeb444925b9c28ff84de56ea977ac9
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-01 14:48:09 +0530

    create checkout component

---
+++ src/Components/CheckOutDetails/CheckOutDetails.jsx
@@ -0,0 +1,37 @@
+import "./CheckOutDetails.css";
+
+const CheckOutDetails = ({ cart, totalPrice }) => {
+  return (
+    <div className="cart_details mx_5">
+      <h2 className="h2">Price details</h2>
+      <hr className="hr" />
+      <div className="price flex">
+        <p className="txt_regular txt_md">Total Items</p>
+        <p className="txt_regular txt_md">{cart.length}</p>
+      </div>
+
+      {cart.map((cartItem) => {
+        return (
+          <div className="price flex" key={cartItem.id}>
+            <p className="txt_regular txt_md">{cartItem.name}</p>
+            <p className="txt_regular txt_md">
+              {cartItem.price * cartItem.qty}
+            </p>
+          </div>
+        );
+      })}
+
+      <hr className="hr" />
+      <div className="total flex">
+        <p className="txt_bold txt_md">Total Amount</p>
+        <p className="txt_bold txt_md">{totalPrice}</p>
+      </div>
+      <hr className="hr" />
+      <button className="btn btn_checkout btn_primary btn_lg ">
+        Place order
+      </button>
+    </div>
+  );
+};
+
+export { CheckOutDetails };
