3472c04d60c57d0c95af2a860f5f4e5211db22f0
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-01 14:40:34 +0530

    Move Navbar component from App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { HomeScreen } from "./Screens";
+import { CartScreen, HomeScreen } from "./Screens";
 import { ProductScreen } from "./Screens";
 import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./Components";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomeScreen />} />
-      <Route path="/productListing" element={<ProductScreen />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/productListing" element={<ProductScreen />} />
+        <Route path="/cart" element={<CartScreen />} />
+      </Routes>
+    </>
   );
 };
 
