ff043b959f8db9127dca85ed3441ea33693dcab8
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-03 01:12:39 +0530

    add check to category fetch

--- src/Components/Categories/CategoryItem.jsx
+++ src/Components/Categories/CategoryItem.jsx
@@ -11,31 +11,44 @@ const CategoryItem = () => {
 
   const fetchCategories = async () => {
     try {
-      const categoriesData = await axios.get("/api/categories");
-      dispatch({ type: "CATEGORIES", payload: categoriesData.data.categories });
+      const categoriesData = await axios.get("/api/categorie");
+      if (categoriesData.status === 200) {
+        dispatch({
+          type: "CATEGORIES",
+          payload: categoriesData.data.categories,
+        });
+      }
     } catch (error) {
+      dispatch({
+        type: "CATEGORIES",
+        payload: null,
+      });
       console.error(error);
     }
   };
 
   useEffect(() => {
-    if (categories.length === 0) fetchCategories();
+    if (categories?.length === 0) fetchCategories();
   }, []);
 
   return (
     <div className="category_items flex">
-      {categories.map((categoryItem) => (
-        <div className="category_product" key={categoryItem._id}>
-          <img
-            src={categoryItem.image}
-            alt={categoryItem.categoryName}
-            className="category_product__image"
-          />
-          <div className="overlay flex_justify__center flex_align__center">
-            <h3 className="h3">{categoryItem.categoryName}</h3>
+      {categories === null ? (
+        <h2 style={{ color: "red" }}>Problem loading categories ...</h2>
+      ) : (
+        categories.map((categoryItem) => (
+          <div className="category_product" key={categoryItem._id}>
+            <img
+              src={categoryItem.image}
+              alt={categoryItem.categoryName}
+              className="category_product__image"
+            />
+            <div className="overlay flex_justify__center flex_align__center">
+              <h3 className="h3">{categoryItem.categoryName}</h3>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
