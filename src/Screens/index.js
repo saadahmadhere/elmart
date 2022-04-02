710e6db85e1cfbd5d7ae75cbac9855b381a7731c
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-02 23:38:47 +0530

    add barrel exports

--- src/Screens/index.js
+++ src/Screens/index.js
@@ -2,3 +2,5 @@ export { ProductScreen } from "./ProductsScreen/ProductsScreen";
 export { HomeScreen } from "./HomeScreen/HomeScreen";
 export { CartScreen } from "./CartScreen/CartScreen";
 export { EmptyCart } from "./Empty Cart/EmptyCart";
+export { WishlistScreen } from "./Wishlist/WishlistScreen";
+export { EmptyWishlist } from "./Empty Wishlist/EmptyWishlist";
