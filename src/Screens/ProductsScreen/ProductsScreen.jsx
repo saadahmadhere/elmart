ce353105c36bd5267742bee4d4f2df97d08a7ece
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-06 01:29:55 +0530

    update product screen

--- src/Screens/ProductsScreen/ProductsScreen.jsx
+++ src/Screens/ProductsScreen/ProductsScreen.jsx
@@ -1,20 +1,18 @@
-import { Card } from "../../Components";
-import { useData } from "../../Helper";
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { Card } from "../../Components";
+import { useData, useFilter } from "../../Helper";
+import { useState, useEffect } from "react";
+import { Filters } from "../../Components";
 
 const ProductScreen = () => {
   const [loader, setLoader] = useState(false);
-  const {
-    state: { products },
-    dispatch,
-  } = useData();
+  const { filteredData, filterDispatch } = useFilter();
 
   const fetchData = async () => {
     try {
       setLoader(true);
       const productsData = await axios.get("/api/products");
-      dispatch({ type: "PRODUCTS", payload: productsData.data.products });
+      filterDispatch({ type: "PRODUCTS", payload: productsData.data.products });
     } catch (err) {
       console.log(err);
     } finally {
@@ -23,22 +21,23 @@ const ProductScreen = () => {
   };
 
   useEffect(() => {
-    if (products.length === 0) fetchData();
+    if (filteredData.length === 0) fetchData();
   }, []);
 
   return (
-    <>
+    <div className="main_filter">
       {loader ? <h1 style={{ fontSize: "20rem" }}>loading</h1> : null}
-      <h2>Hello</h2>
-
-      <div
-        className="flex flex_wrap flex_justify__center"
-        style={{ gap: "2rem" }}>
-        {products.map((product) => (
-          <Card product={product} key={product._id} />
-        ))}
-      </div>
-    </>
+      <Filters />
+      <section className="products mt_10">
+        <div
+          className="product_list flex flex_wrap flex_justify__center"
+          style={{ gap: "2rem" }}>
+          {filteredData.map((product) => (
+            <Card product={product} key={product._id} />
+          ))}
+        </div>
+      </section>
+    </div>
   );
 };
 
