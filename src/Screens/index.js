2dc3e5c3b32f3e403d039fdcddecd0af900268e5
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-06 23:30:49 +0530

    barrel exports

--- src/Screens/index.js
+++ src/Screens/index.js
@@ -4,3 +4,5 @@ export { CartScreen } from "./CartScreen/CartScreen";
 export { EmptyCart } from "./Empty Cart/EmptyCart";
 export { WishlistScreen } from "./Wishlist/WishlistScreen";
 export { EmptyWishlist } from "./Empty Wishlist/EmptyWishlist";
+export { Login } from "./Login Screen/Login";
+export { SignUp } from "./SignUp/SignUp";
