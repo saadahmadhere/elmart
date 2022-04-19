f80e2cf4007c1f9f4dcd13f2e09b44107dac20db
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-20 05:20:09 +0530

    feat: add test login

--- src/Screens/Login Screen/Login.jsx
+++ src/Screens/Login Screen/Login.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import "./login.css";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const emailHandler = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const passwordHandler = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const showPasswordHandler = (e) => {
+    e.preventDefault();
+    setShowPassword((prev) => !prev);
+  };
+
+  const testLoginHandler = (e) => {
+    e.preventDefault();
+    setEmail("adarshbalika@gmail.com");
+    setPassword("adarshbalika");
+  };
+
   return (
     <main className="login flex_col flex_align__center">
       <h1 className="h2 gray_title">Login</h1>
@@ -12,8 +38,10 @@ const Login = () => {
         <input
           id="email_id"
           type="email"
-          placeholder="abc@example.com"
+          placeholder="si@senor.com"
           className="input block"
+          value={email}
+          onChange={emailHandler}
           required
         />
         <div className="login_pwd">
@@ -24,23 +52,28 @@ const Login = () => {
             Password
           </label>
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             id="password"
             className="input block"
             placeholder="Your password here..."
+            value={password}
+            onChange={passwordHandler}
             required
           />
-          <a className="btn_icon btn_show__password">
+          <button
+            className="btn btn_show__password"
+            onClick={showPasswordHandler}>
             <span className="material-icons outlined visibility ">
               visibility_off
             </span>
-          </a>
-        </div>
-        <div className="remember_me">
-          <input type="checkbox" id="check_box" />
-          <label htmlFor="check_box">Remember me</label>
+          </button>
         </div>
         <button className="btn btn_primary btn_sm txt_semibold">Login</button>
+        <button
+          className="btn btn_outline btn_sm mt_2"
+          onClick={testLoginHandler}>
+          Login with test credentials
+        </button>
         <a className="txt_sm forgot_pwd">Forgotten Password?</a>
         <hr className="hr" />
         <Link to="/signUp" className="btn btn_create__acc btn_sm txt_semibold">
