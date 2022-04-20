f115c2afabe9c3737382b38916976d6e89ff586b
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-20 06:59:22 +0530

    feat: add AuthProvider

--- src/index.js
+++ src/index.js
@@ -4,20 +4,26 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ProductDataProvider, FilterContextProvider } from "./Helper";
+import {
+  ProductDataProvider,
+  FilterContextProvider,
+  AuthProvider,
+} from "./Helper";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <FilterContextProvider>
-        <ProductDataProvider>
-          <App />
-        </ProductDataProvider>
-      </FilterContextProvider>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <FilterContextProvider>
+          <ProductDataProvider>
+            <App />
+          </ProductDataProvider>
+        </FilterContextProvider>
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
