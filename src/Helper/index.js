a9a514cafa2a3c6bd9080f02cadabdd125f8941d
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-20 06:59:52 +0530

    update: barrel exports

--- src/Helper/index.js
+++ src/Helper/index.js
@@ -2,3 +2,4 @@ export { useData, ProductDataProvider } from "./Contexts/data-context";
 export { FilterContextProvider, useFilter } from "./Contexts/filters-context";
 export { filterReducer } from "./Reducers/filter-reducer";
 export { useFixSidebarOverflow } from "./Hooks/use-Fix-Sidebar-Overflow";
+export { useAuth, AuthProvider } from "./Contexts/auth-context";
