e14d398edddba37855b47127a491c1dc54370132
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-28 05:11:32 +0530

    feat: auth context and reducer

--- src/Helper/Contexts/auth-context.js
+++ src/Helper/Contexts/auth-context.js
@@ -1,14 +1,26 @@
-import { createContext, useContext, useState } from "react";
-
+import { createContext, useContext, useReducer } from "react";
+import { userDataReducer } from "../Reducers/user-data-reducer";
 const AuthContext = createContext();
 
 const useAuth = () => useContext(AuthContext);
 
 const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userState, dispatchUserState] = useReducer(userDataReducer, {
+    token: localStorage.getItem("token"),
+    userData: JSON.parse(localStorage.getItem("userData")) ?? {
+      cart: [],
+      wishlist: [],
+    },
+  });
+
+  const inCart = (id) =>
+    userState?.userData?.cart?.find((cartItem) => cartItem._id === id);
+  const inWishlist = (id) =>
+    userState?.userData?.wishlist?.find((wishlist) => wishlist._id === id);
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+    <AuthContext.Provider
+      value={{ userState, dispatchUserState, inCart, inWishlist }}>
       {children}
     </AuthContext.Provider>
   );
